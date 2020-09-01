4d49f39187995e4b9c4a1bc569fd788abd9fc045
Garritt <gmoede@wisc.edu>
2020-09-01 00:12:17 -0500

    stringer formatting

--- schedule.go
+++ schedule.go
@@ -58,6 +58,7 @@ func (sch Schedule) String() string {
 		for k, v := range sch.scheduler.fairMap {
 			fmt.Fprintf(writer, "participant [%s] weeks [%d]\n", k, v)
 		}
+		fmt.Fprintln(writer)
 	}
 	writer.Flush()
 	return b.String()
